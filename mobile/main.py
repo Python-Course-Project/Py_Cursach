02452d82ba10ecce75fa0a81cfe9556bd4a99216
Frumka <Hekchanel@mail.ru>
2020-07-08 07:38:05 +0300

    BUTTONS DONT FREEZE!!!!!!

--- mobile/main.py
+++ mobile/main.py
@@ -6,6 +6,15 @@ from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty, StringProperty
+# noinspection PyUnresolvedReferences
+from mobile.MainScreen import MainScreen
+from mobile.connection_controller.ConnectionController import ConnectionController
+from requests.exceptions import HTTPError, ConnectionError
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivy.uix.screenmanager import WipeTransition, SwapTransition, SlideTransition
+from kivy.clock import Clock
+
 Window.size = (300, 500)
 
 
@@ -13,16 +22,112 @@ class LoginScreenManager(ScreenManager):
     pass
 
 
+# class HiScreen(Screen):
+#     def automatic_switch(self):
+#         print("HI")
+#         Clock.schedule_once(self.switch_to_auth, 2)
+#
+#     def switch_to_auth(self):
+#         print("!!!!!!!!!!!!!!!!!!!!!!")
+#         app = MDApp.get_running_app()
+#         app.root.current = "AuthScreen"
+
 class AuthScreen(Screen):
     login_input = ObjectProperty()
     password_input = ObjectProperty()
 
+    # TODO: автозаполнение
+    # def fill_on_enter(self):
+    #     self.login_input.text = ConnectionController().username
+
+
     def verify_auth(self):
-        print(self.login_input.text, self.password_input.text)
+        if not self.login_input.text:
+            dialog = MDDialog(title="Login", text="Empty login rejected", size_hint=(0.8, None))
+            dialog.open()
+            return False
+
+        if not self.password_input.text:
+            dialog = MDDialog(title="Login", text="Empty password rejected", size_hint=(0.8, None))
+            dialog.open()
+            return False
+
+        return True
+
+    def log_in(self):
+        try:
+            ConnectionController.login(self.login_input.text, self.password_input.text)
+        except ConnectionError:
+            dialog = MDDialog(title="Login", text="Network failed you(", size_hint=(0.8, None))
+            dialog.open()
+
+            return False
+        except HTTPError:
+            dialog = MDDialog(title="Login", text="Wrong username/password \n Forgot password? Bad.",
+                              size_hint=(0.8, None))
+            dialog.open()
+
+            return False
+
+        return True
+
+    def login_button_behavior(self):
+        if self.verify_auth():
+            if self.log_in():
+                self.manager.transition = SlideTransition()
+                self.manager.direction = 'up'
+                self.manager.current = "MainScreen"
+
+
+
+
 
 
 class RegisterScreen(Screen):
-    pass
+    login_input = ObjectProperty()
+    password_input = ObjectProperty()
+    password_confirm_input = ObjectProperty()
+
+    def verify_fields(self):
+        if not self.login_input.text:
+            dialog = MDDialog(title="Register", text="Empty login rejected", size_hint=(0.8, None))
+            dialog.open()
+            return False
+
+        if not self.password_input.text:
+            dialog = MDDialog(title="Register", text="Empty password rejected", size_hint=(0.8, None))
+            dialog.open()
+            return False
+
+        if self.password_input.text != self.password_confirm_input.text:
+            dialog = MDDialog(title="Register", text="Passwords do not match", size_hint=(0.8, None))
+            dialog.open()
+            return False
+
+        return True
+
+    def register(self):
+        try:
+            ConnectionController.register(self.login_input.text, self.password_input.text)
+        except ConnectionError:
+            dialog = MDDialog(title="Register", text="Network failed you(", size_hint=(0.8, None))
+            dialog.open()
+
+            return False
+        except HTTPError:
+            dialog = MDDialog(title="Register", text="User already exist", size_hint=(0.8, None))
+            dialog.open()
+
+            return False
+
+        return True
+
+    def register_button_behavior(self):
+        if self.verify_fields():
+            if self.register():
+                self.manager.transition = SwapTransition()
+                self.manager.current = "AuthScreen"
+
 
 
 class MainApp(MDApp):
